0f920ab9d3089c32cadff056aabf7ac11abd2748
suphakin-th <suphakin.th@gmail.com>
2025-02-09 22:46:39 +0700

    Feature : Add  Facade structure for better codding.

---
+++ base/src/error.rs
@@ -0,0 +1,60 @@
+use std::io;
+
+use config::ConfigError;
+use poisson::ctx::ContextError;
+use reqwest::{
+	header::{InvalidHeaderValue, ToStrError},
+	Error as ReqErr,
+};
+use snafu::{prelude::*, Report};
+use time::error::ComponentRange;
+use tokio::{
+	sync::broadcast::error::{RecvError, SendError},
+	task::JoinError,
+};
+use tracing::metadata::ParseLevelFilterError;
+
+#[derive(Debug, Snafu)]
+#[snafu(visibility(pub))]
+pub enum Error {
+	#[snafu(display("config error"))]
+	ConfigEnv { source: ConfigError },
+	#[snafu(display("path error"))]
+	PathEnv { source: io::Error },
+	#[snafu(display("set oncecell tokio pg error"))]
+	LevelFilterError { source: ParseLevelFilterError },
+	#[snafu(display("unsupport env"))]
+	UnsupportEnv,
+	#[snafu(display("tracing global default error"))]
+	GlobalDefautError,
+	#[snafu(display("pg error"))]
+	TokioPgError { source: tokio_postgres::Error },
+	#[snafu(display("overflow error"))]
+	Overflow,
+	#[snafu(display("serde json error"))]
+	SerdeJsonError { source: serde_json::Error },
+	#[snafu(display("invalid timestamp"))]
+	NaiveDateTimeError,
+	#[snafu(display("invalid decimal"))]
+	DecimalError,
+	#[snafu(display("reconnect error"))]
+	ReconnectError,
+	#[snafu(display("External service Request error"))]
+	ExtSvcRequestError { source: reqwest::Error },
+	#[snafu(display("reqwest error"))]
+	ReqwestError { source: ReqErr },
+	#[snafu(display("reqwest clone error"))]
+	ReqwestCloneError,
+	#[snafu(display("reqwest header value error"))]
+	ReqwestHeaderValueError {
+		source: reqwest::header::InvalidHeaderValue,
+	},
+	#[snafu(display("Unable to create interval period"))]
+	PeriodError,
+}
+
+impl Error {
+	pub fn report(&self) {
+		tracing::error!("error: error_msg {}", Report::from_error(self))
+	}
+}
--- open_rust_map/src/main.rs
+++ open_rust_map/src/main.rs
@@ -1,3 +1,24 @@
+use osm_pbf::{PbfReader, Blob};
+
 fn main() {
-    println!("Hello, world!");
+    let file = std::fs::File::open("data /thailand.pbf")?;
+    let mut reader = PbfReader::new(file);
+
+    while let Some(blob) = reader.next_blob()? {
+        match blob {
+            Blob::OSMData(data) => {
+                // Process OSM data here
+                println!("OSM data: {:?}", data);
+            }
+            Blob::OSMHeader(header) => {
+                // Process OSM header here
+                println!("OSM header: {:?}", header);
+            }
+            _ => {
+                // Ignore other blob types
+            }
+        }
+    }
+
+    Ok(())
 }
